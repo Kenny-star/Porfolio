05b51e85671541190eb1d82f633d1aea3f9e3bfa
Kenny <54686876+Kenny-star@users.noreply.github.com>
2024-08-19 17:28:40 -0400

    projects navigation

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,6 +3,7 @@ import useLocalStorage from '../components/useLocalStorage';
 import { useSpring, animated } from '@react-spring/web';
 import { useDrag } from '@use-gesture/react';
 import { useState, useRef, useEffect } from 'react';
+import { useTheme } from '../components/ThemeContext';
 
 const Projects = () => {
     const [counter, setCounter] = useLocalStorage('counter', 0);
@@ -12,39 +13,42 @@ const Projects = () => {
     const [selectedImage, setSelectedImage] = useState('');
     const dragging = useRef(false);
 
+    const {theme} = useTheme();
     // TO CHANGE
-    // const nextPicture = () => {
-    //     setCounter((prevCounter) => (prevCounter + 1) % projects.length);
-    // };
+    const nextPicture = () => {
+        setAlbumInCounter((prevCounter) => (prevCounter + 1) % projects[counter].album.length);
+    };
 
-    // const prevPicture = () => {
-    //     setCounter((prevCounter) => (prevCounter - 1 + projects.length) % projects.length);
-    // };
+    const prevPicture = () => {
+        setAlbumInCounter((prevCounter) => (prevCounter - 1 + projects[counter].album.length) % projects[counter].album.length);
+    };
 
     const nextProject = () => {
         setCounter((prevCounter) => (prevCounter + 1) % projects.length);
+        setAlbumInCounter(0);
     };
 
     const prevProject = () => {
         setCounter((prevCounter) => (prevCounter - 1 + projects.length) % projects.length);
+        setAlbumInCounter(0);
     };
 
     const bind = useDrag(({ active, movement: [mx], direction: [xDir], cancel }) => {
         dragging.current = active;
         if (active && Math.abs(mx) > window.innerWidth / 2.5) {
             cancel();
-            if (xDir < 0 && counter < projects.length - 1) nextProject();
-            else if (xDir >= 0 && counter > 0) prevProject();
+            if (xDir < 0 && album_in_counter < projects[album_in_counter].album.length - 1) nextPicture();
+            else if (xDir >= 0 && album_in_counter > 0) prevPicture();
         }
         api.start({ x: active ? mx : 0 });
     });
 
     useEffect(() => {
         const handleKeyDown = (event: KeyboardEvent) => {
-            if ((event.key === 'ArrowRight' || event.key === "d") && counter < projects.length - 1) {
-                nextProject();
-            } else if ((event.key === 'ArrowLeft' || event.key === "a") && counter > 0) {
-                prevProject();
+            if ((event.key === 'ArrowRight' || event.key === "d") && album_in_counter < projects[album_in_counter].album.length - 1) {
+                nextPicture();
+            } else if ((event.key === 'ArrowLeft' || event.key === "a") && album_in_counter > 0) {
+                prevPicture();
             }
         };
 
@@ -53,7 +57,7 @@ const Projects = () => {
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
         };
-    }, [counter]);
+    }, [album_in_counter]);
 
     // const getGridClasses = (type: number) => {
     //     switch (type) {
@@ -90,21 +94,37 @@ const Projects = () => {
             >
                 <div className="flex flex-col items-center justify-center bg-slate-400 w-full h-full rounded-l-md overflow-visible cursor-pointer select-none">
                     {/* <div className={`${getGridClasses(projects[counter].album.length)} w-full h-full p-10 `}> */}
-                        {projects[counter].album.map((album, idx2) => (
-                            <div key={idx2} className="relative w-full h-full">
+                    <div className="relative w-full h-full">
+                        {projects.map((_, idx2) => (
+                            <div key={idx2} >
                                 <img
                                     alt={projects[counter].name}
-                                    src={album}
+                                    src={projects[counter].album[album_in_counter]}
                                     className="absolute inset-0 w-full h-full rounded-l-lg pointer-events-auto object-fill "
-                                    onClick={() => openModal(album)}
+                                    onClick={() => openModal(projects[counter].album[album_in_counter])}
                                 />
+                            
                             </div>
                         ))}
+                        {album_in_counter < projects[counter].album.length - 1 && (
+                                <div className="absolute top-1/2 right-[calc(10px+3%)] transform -translate-y-1/2 text-4xl hover:cursor-pointer rounded-2xl bg-slate-400 py-3 px-3.5" onClick={nextPicture}>
+                                    <div className={`w-0 h-0 border-t-[8px] border-t-transparent border-b-[8px] border-b-transparent border-l-[12px] ${theme === 'light' ? 'border-l-white' : 'border-l-gray_blue'} ml-0.5`}/>
+                                </div>
+                            )}
+                            {album_in_counter > 0 && (
+                                <div className="absolute top-1/2 left-[calc(10px+3%)] transform -translate-y-1/2 text-4xl hover:cursor-pointer rounded-2xl bg-slate-400 py-3 px-3.5" onClick={prevPicture}>
+                                    <div className={`w-0 h-0 border-t-[8px] border-t-transparent border-b-[8px] border-b-transparent border-r-[12px]  ${theme === 'light' ? 'border-r-white' : 'border-r-gray_blue'}  mr-0.5`}/>
+                                </div>
+                            )}
+                        </div>
+                        
                     <div className="absolute bottom-0 flex flex-row space-x-1 z-20 p-2">
-                {projects.map((_, index) => (
-                    <div key={index} className={`size-3 rounded-full ${counter === index ? 'bg-blue-500' : 'bg-slate-400'}`}/>
+                {projects[counter].album.map((_, index) => (
+                    <div key={index} className={`size-3 rounded-full ${album_in_counter === index ? 'bg-blue-500' : 'bg-slate-400'}`}/>
                 ))}
             </div>
+
+            
                 </div>
                 <div className="flex justify-center p-10 w-1/4 bg-gray-200 h-full rounded-r-md cursor-default">
                     {/* <a href="https://google.ca"> yeey</a> */}
@@ -126,13 +146,17 @@ const Projects = () => {
             )}
             
             {counter < projects.length - 1 && (
-                <div className="absolute top-1/2 -right-3 md:right-[calc(10px)] lg:right-[calc(10px+2%)] xl:right-[calc(10px+3%)] transform -translate-y-1/2 text-4xl hover:cursor-pointer" onClick={nextProject}>
-                    ▶
+                <div className="absolute top-1/2 right-[calc(10px+3%)] transform -translate-y-1/2 text-4xl hover:cursor-pointer rounded-2xl bg-slate-400 py-3 px-3.5" onClick={nextProject}>
+                    <div className={`w-0 h-0 border-t-[8px] border-t-transparent border-b-[8px] border-b-transparent border-l-[12px] ${theme === 'light' ? 'border-l-white' : 'border-l-gray_blue'} ml-0.5`}/>
                 </div>
             )}
             {counter > 0 && (
-                <div className="absolute top-1/2 -left-3 md:left-[calc(10px)] lg:left-[calc(10px+2%)] xl:left-[calc(10px+3%)] transform -translate-y-1/2 text-4xl hover:cursor-pointer" onClick={prevProject}>
-                    ◀
+                // <div className="absolute top-1/2 left-[calc(10px+3%)] transform -translate-y-1/2 text-4xl hover:cursor-pointer" onClick={prevProject}>
+                //     ◀
+                // </div>
+
+                <div className="absolute top-1/2 left-[calc(10px+3%)] transform -translate-y-1/2 text-4xl hover:cursor-pointer rounded-2xl bg-slate-400 py-3 px-3.5" onClick={prevProject}>
+                    <div className={`w-0 h-0 border-t-[8px] border-t-transparent border-b-[8px] border-b-transparent border-r-[12px]  ${theme === 'light' ? 'border-r-white' : 'border-r-gray_blue'}  mr-0.5`}/>
                 </div>
             )}
         </div>
